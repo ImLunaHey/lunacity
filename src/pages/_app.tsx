990c2f53b4c91696cd60c340ee6310834776f035
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-04 12:13:40 +1030

    chore: add nextui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,35 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { type AppType } from 'next/app';
+import { type Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
 
-import { api } from "../utils/api";
+import { api } from '../utils/api';
 
-import "../styles/globals.css";
+import '../styles/globals.css';
+import Head from 'next/head';
+import Navbar from '../components/navbar';
+
+// 2. Call `createTheme` and pass your custom theme values
+const theme = createTheme({
+  type: 'dark'
+});
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <NextUIProvider theme={theme}>
+      <SessionProvider session={session} refetchInterval={1}>
+        <Head>
+          <title>Social Media Site</title>
+          <meta name="description" content="Social Media site" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Navbar {...pageProps} />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </NextUIProvider>
   );
 };
 
