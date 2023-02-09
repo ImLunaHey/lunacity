5a53d6d50f802fefc7d579739214ef4f7a39064b
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-09 11:43:49 +1030

    feat: add tags page

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -6,25 +6,9 @@ const Tags = () => {
     const tags = api.tag.getAllTags.useQuery();
 
     // Show loading while we fetch data
-    if (tags.isLoading) return (
-        <main className="flex flex-col items-center justify-center">
-            <div className="container flex flex-col items-center justify-center gap-12 px-4 py-4 pt-4">
-                <div className="flex flex-col items-center justify-center gap-4">
-                    <Loading />
-                </div>
-            </div>
-        </main>
-    );
+    if (tags.isLoading) return (<Loading />);
 
-    return (
-        <main className="flex flex-col items-center justify-center">
-            <div className="container flex flex-col items-center justify-center gap-12 px-4 py-4 pt-4">
-                <div className="flex flex-col items-center justify-center gap-4">
-                <TagCloud tags={tags.data ?? []} />
-                </div>
-            </div>
-        </main>
-    );
+    return (<TagCloud tags={tags.data ?? []} />);
 }
 
 export default Tags;
