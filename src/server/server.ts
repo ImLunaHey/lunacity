6658479839e6fc64573bc5080fc450459ef758c2
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-09 11:41:22 +1030

    feat: add websocket server

---
+++ src/server/server.ts
@@ -0,0 +1,42 @@
+import 'dotenv/config';
+
+import { createWebsocketContext } from '@app/server/api/trpc';
+import { applyWSSHandler } from '@trpc/server/adapters/ws';
+import http from 'http';
+import next from 'next';
+import { parse } from 'url';
+import ws from 'ws';
+import { appRouter } from './api/root';
+
+const port = parseInt(process.env.PORT || '3001', 10);
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(async () => {
+    const server = http.createServer((req, res) => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const parsedUrl = parse(req.url!, true);
+        handle(req, res, parsedUrl);
+    });
+    const wss = new ws.Server({ server });
+    const handler = applyWSSHandler({
+        wss,
+        router: appRouter,
+        async createContext(opts) {
+            return createWebsocketContext(opts);
+        }
+    });
+
+    process.on('SIGTERM', () => {
+        console.log('SIGTERM');
+        handler.broadcastReconnectNotification();
+    });
+    server.listen(port);
+
+    // tslint:disable-next-line:no-console
+    console.log(
+        `> Server listening at http://localhost:${port} as ${dev ? 'development' : process.env.NODE_ENV
+        }`,
+    );
+});
