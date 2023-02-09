cc019392a29f750ea2d641da9859aceccdd24395
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-09 22:12:53 +1030

    fix: [postId] not being passed postId

--- src/pages/post/[postId].tsx
+++ src/pages/post/[postId].tsx
@@ -1,10 +1,23 @@
-import type { NextPage } from 'next';
-import { SinglePost } from '../../components/single-post';
-import { api } from '../../utils/api';
+import type { GetServerSidePropsContext, NextPage } from 'next';
+import { SinglePost } from '@app/components/single-post';
+import { api } from '@app/utils/api';
+import { Loading } from '@nextui-org/react';
+import Error from 'next/error';
 
-const Post: NextPage<{ postId: string; }> = ({ postId }) => {
-    const post = api.post.getPostDetails.useQuery({ postId });
-    return (<SinglePost {...post.data as any} />);
+// Return the handle from the url
+export function getServerSideProps(context: GetServerSidePropsContext<{ postId: string }>) {
+  return {
+    props: {
+      postId: context.query.postId,
+    },
+  };
+}
+
+const Post: NextPage<{ postId: string }> = ({ postId }) => {
+  const post = api.post.getPostDetails.useQuery({ postId });
+  if (post.isLoading) return <Loading />;
+  if (!post.data) return <Error statusCode={404} title="No post exists with this ID"></Error>;
+  return <SinglePost {...post.data} />;
 };
 
 export default Post;
