8ab2e4e6c0dd00f21e747e64b5138872a3337741
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-09 11:47:49 +1030

    feat: add handle and page to user's session

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,3 +1,4 @@
+import { Page } from '@prisma/client';
 import { type DefaultSession } from 'next-auth';
 
 declare module 'next-auth' {
@@ -7,6 +8,8 @@ declare module 'next-auth' {
   interface Session {
     user?: {
       id: string;
+      handle?: string;
+      page?: Page
     } & DefaultSession['user'];
   }
 }
