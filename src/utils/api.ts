91d0452d713bbd0d3f9a642f2edb008347b24ddd
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-09 11:16:49 +1030

    feat: add websocket support to trpc

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -5,24 +5,70 @@
  *
  * We also create a few inference helpers for input and output types
  */
-import { httpBatchLink, loggerLink } from '@trpc/client';
+import { createWSClient, httpBatchLink, loggerLink, splitLink, wsLink } from '@trpc/client';
 import { createTRPCNext } from '@trpc/next';
 import { type inferRouterInputs, type inferRouterOutputs } from '@trpc/server';
+import { NextPageContext } from 'next';
+import getConfig from 'next/config';
 import superJSON from 'superjson';
+import { AppRouter } from '../server/api/root';
 
-import { type AppRouter } from '../server/api/root';
+const { publicRuntimeConfig } = getConfig();
+const { APP_URL, WS_URL } = publicRuntimeConfig;
+const getEndingLink = (ctx: NextPageContext | undefined) => {
+  if (typeof window === 'undefined') {
+    return httpBatchLink({
+      url: `${APP_URL}/api/trpc`,
+      headers() {
+        if (ctx?.req) {
+          // on ssr, forward client's headers to the server
+          return {
+            ...ctx.req.headers,
+            'x-ssr': '1',
+          };
+        }
+        return {};
+      },
+    });
+  }
 
-const getBaseUrl = () => {
-  if (typeof window !== 'undefined') return ''; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-};
+  // Make Websocket client
+  const client = createWSClient({
+    url: WS_URL,
+  });
+
+  // If we're on the client create a split link that sends both queries
+  // and mutations over http-batch and all subscriptions over websocket
+  return splitLink({
+    condition(op) {
+      return op.type !== 'subscription';
+    },
+    // HTTP batching
+    true: httpBatchLink({
+      url: `${APP_URL}/api/trpc`,
+      headers() {
+        if (ctx?.req) {
+          // on ssr, forward client's headers to the server
+          return {
+            ...ctx.req.headers,
+            'x-ssr': '1',
+          };
+        }
+        return {};
+      },
+    }),
+    // Websocket
+    false: wsLink<AppRouter>({
+      client,
+    })
+  });
+}
 
 /**
  * A set of typesafe react-query hooks for your tRPC API
  */
 export const api = createTRPCNext<AppRouter>({
-  config() {
+  config({ ctx }) {
     return {
       /**
        * Transformer used for data de-serialization from the server
@@ -40,17 +86,15 @@ export const api = createTRPCNext<AppRouter>({
             process.env.NODE_ENV === 'development' ||
             (opts.direction === 'down' && opts.result instanceof Error),
         }),
-        httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
-        }),
-      ],
+        getEndingLink(ctx)
+      ]
     };
   },
   /**
    * Whether tRPC should await queries when server rendering pages
    * @see https://trpc.io/docs/nextjs#ssr-boolean-default-false
    */
-  ssr: false,
+  ssr: true,
 });
 
 /**
