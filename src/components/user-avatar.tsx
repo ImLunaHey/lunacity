e69c46adcf717d01a53010ad2df9efed2eafc675
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-09 11:14:41 +1030

    feat: add user-avatar component

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -1,14 +1,24 @@
 import { User } from '@nextui-org/react';
+import Link from 'next/link';
 import type { FC } from 'react';
 
-export const UserAvatar: FC<{ src: string; name: string; handle: string; official: boolean; }> = ({ handle, name, src, official }) => {
-    return (
-        <User
-            bordered
-            color={official ? 'secondary' : 'primary'}
-            altText={`Avatar for ${name}`}
-            src={src}
-            name={name}
-        ><User.Link href={`/@${handle}`} target={'_self'}>@{handle}</User.Link></User>
-    );
-}
+export const UserAvatar: FC<{
+  src: string;
+  name: string;
+  handle: string;
+  official: boolean;
+}> = ({ handle, name, src, official }) => {
+  return (
+    <User
+      bordered
+      color={official ? 'secondary' : 'primary'}
+      altText={`Avatar for ${name}`}
+      src={src}
+      name={name}
+    >
+      <User.Link href={`/@${handle}`} target={'_self'} as={Link}>
+        @{handle}
+      </User.Link>
+    </User>
+  );
+};
