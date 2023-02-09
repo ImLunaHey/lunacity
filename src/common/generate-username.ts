3c7ab5ec7eee0f119e069b9e41bb440733d88f87
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-09 11:13:42 +1030

    feat: add generate-username helper

---
+++ src/common/generate-username.ts
@@ -0,0 +1,7 @@
+import { uniqueNamesGenerator, adjectives, colors, animals } from 'unique-names-generator';
+
+export const generateUsername = () => uniqueNamesGenerator({
+    dictionaries: [adjectives, colors, animals],
+    separator: '-',
+    seed: 120498,
+});
