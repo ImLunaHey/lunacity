2bc1c5485b35ca0ecb33fa4cf483221685c8648f
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-09 11:44:36 +1030

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,68 @@
 import { type NextPage } from 'next';
 import Head from 'next/head';
 import { signIn, useSession } from 'next-auth/react';
-import { Button } from '@nextui-org/react';
-import PageSelector from '../components/page-selector';
+import { Button, Loading, Spacer } from '@nextui-org/react';
 import Link from 'next/link';
+import { api } from '../utils/api';
+import Feed from '../components/feed';
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
 
-const Home: NextPage = () => {
-  const { data: sessionData } = useSession();
+export const UnauthenticatedPage: FC = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <Button onClick={() => void signIn()}>{t('signin')}</Button>
+      </div>
+    </div>
+  );
+};
 
-  // Unauthenticated user
-  if (!sessionData) {
-    return (
-      <>
-        <Head>
-          <title>Social Network</title>
-        </Head>
-        <main className="flex min-h-screen flex-col items-center justify-center">
-          <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-            <div className="flex flex-col items-center justify-center gap-4">
-              <Button onClick={() => void signIn()}>Signin</Button>
-            </div>
-          </div>
-        </main>
-      </>
-    );
-  }
+export const AuthenticatedPage: FC = () => {
+  const { t } = useTranslation();
+  const posts = api.user.getAllPosts.useQuery();
 
-  // Authenticated user
+  // Show loading while we fetch data
+  if (posts.isLoading) return <Loading />;
+
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
-      <main className="flex flex-col items-center justify-center container gap-12 px-4 py-4 pt-4">
-        {/* Might be a better way to align this using flex */}
-        {/* This will be a + for adding a new page */}
-        <div className='container px-5'>
-          <Link className="p-2 bg-white text-black rounded-md float-right" href="/page/create">Create a new page</Link>
+      <div className="flex flex-col">
+        <div className="flex flex-row-reverse">
+          <Link
+            className="rounded-md bg-white p-2 text-black"
+            href="/post/create"
+          >
+            {t('create-post')}
+          </Link>
         </div>
-        {/* TODO: We want card buttons to show the pages this user has access to */}
-        <PageSelector />
-      </main>
+        <Spacer y={0.5} />
+        <Feed
+          fetchData={(opts) => () => undefined}
+          className="clear-both mt-2"
+          // @TODO: Fix this, currently the types arent using the trpc returned types
+          items={posts.data ?? []}
+        />
+      </div>
     </>
-  )  
+  );
+};
+
+const Home: NextPage = () => {
+  const { status } = useSession();
+
+  // Show loading while we fetch data
+  if (status === 'loading') return <Loading />;
+
+  // Unauthenticated user
+  if (status === 'unauthenticated') return <UnauthenticatedPage />;
+
+  // Authenticated user
+  return <AuthenticatedPage />;
 };
 
 export default Home;
