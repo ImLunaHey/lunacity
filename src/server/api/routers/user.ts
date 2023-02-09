148bdcbd09ed8d933e292212e7caa165760272fd
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-09 11:46:55 +1030

    feat: add user trpc route

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,110 @@
+import { TRPCError } from '@trpc/server';
+import { z } from 'zod';
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+
+export const userRouter = createTRPCRouter({
+  // infinitePosts: t
+  //   .procedure
+  //   .input(z.object({
+  //     limit: z.number().min(1).max(100).nullish(),
+  //     cursor: z.number().nullish(), // <-- "cursor" needs to exist, but can be any type
+  //   }))
+  //   .query(({ input }) => {
+  //     const limit = input.limit ?? 50;
+  //     const { cursor } = input;
+  //     const items = await prisma.post.findMany({
+  //       take: limit + 1, // get an extra item at the end which we'll use as next cursor
+  //       where: {
+  //         title: {
+  //           contains: 'Prisma' /* Optional filter */,
+  //         },
+  //       },
+  //       cursor: cursor ? { myCursor: cursor } : undefined,
+  //       orderBy: {
+  //         myCursor: 'asc',
+  //       },
+  //     })
+  //     let nextCursor: typeof cursor | undefined = undefined;
+  //     if (items.length > limit) {
+  //       const nextItem = items.pop()
+  //       nextCursor = nextItem!.myCursor;
+  //     }
+  //     return {
+  //       items,
+  //       nextCursor,
+  //     };
+  //   })
+
+  getAllPosts: publicProcedure.query(async ({ ctx: { prisma, session } }) => {
+    // Unauthenticated user
+    if (!session?.user) return [];
+
+    // Get the current session's page
+    const sessionPage = await prisma.page.findUnique({
+      where: {
+        ownerId: session.user.id
+      }
+    });
+
+    // No page found for the current session
+    if (!sessionPage)
+      throw new TRPCError({
+        code: 'NOT_FOUND',
+        message: 'No page found for your session.',
+      });
+
+    // Authenticated user
+    return prisma?.post.findMany({
+      where: {
+        page: {
+          followedBy: {
+            some: {
+              followerId: sessionPage.id,
+            },
+          },
+        },
+      },
+      include: {
+        tags: true,
+        page: {
+          include: {
+            owner: true,
+          },
+        },
+        media: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+  }),
+
+  updateSettings: protectedProcedure.input(z.object({
+    handle: z.string().optional(),
+    email: z.string().optional()
+  })).mutation(async ({ ctx: { prisma, session }, input }) => {
+
+    // If they're trying to update their handle
+    if (input.handle) {
+      const existingUserWithHandle = await prisma.user.findUnique({
+        where: {
+          handle: input.handle
+        }
+      })
+    }
+
+    await prisma.user.update({
+      where: {
+        id: session.user.id
+      },
+      data: {
+        handle: input.handle,
+        page: {
+          update: {
+            handle: input.handle
+          }
+        }
+      }
+    })
+  })
+});
