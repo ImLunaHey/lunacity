65bb9ddd4cedb43d60d40458cf007dd7f195021e
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-09 22:10:14 +1030

    chore: allow 120 wide code instead of 80

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
-  singleQuote: true
+  singleQuote: true,
+  printWidth: 120
 };
