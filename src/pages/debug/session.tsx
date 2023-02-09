cd42987d91a263db40ca0224cb48b87e0865f9a4
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-09 11:41:47 +1030

    feat: add debug session page

---
+++ src/pages/debug/session.tsx
@@ -0,0 +1,14 @@
+import { Loading } from '@nextui-org/react';
+import { useSession } from 'next-auth/react';
+import type { FC } from 'react';
+
+// @TODO: This should only work for devs
+//        We need to make it so the user has a field that has this
+const Settings: FC = () => {
+  const { data, status } = useSession();
+
+  if (status === 'loading') return (<Loading />);
+  return (<pre>{JSON.stringify(data, null, 2)}</pre>);
+};
+
+export default Settings;
