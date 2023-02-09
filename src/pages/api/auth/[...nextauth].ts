6a81ea2f8e149d7842ceebdd569b8a6ff3e543c1
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-09 11:53:45 +1030

    fix: ensure user has an associated page created on first signin

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,16 +1,80 @@
-import NextAuth, { type NextAuthOptions } from 'next-auth';
+import NextAuth, { User, type NextAuthOptions } from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
-
 import { env } from '../../../env/server.mjs';
 import { prisma } from '../../../server/db';
+import { generateUsername } from '../../../common/generate-username';
+
+const allowedEmails = ['staff@example.com', 'test@example.com'];
+
+// @TODO: make this not shit code
+// WARNING: BAD CODE IN THIS WHOLE BLOCK
+const handleNewUser = async (user: User) => {
+  // Attempt to create a page with a generated handle
+  let handle = generateUsername();
+  let errorCount = 5;
+  while (true) {
+    try {
+      const generatedHandle = handle ?? generateUsername();
+      await prisma.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          handle: generatedHandle,
+          page: {
+            create: {
+              handle: generatedHandle,
+              displayName: `@${generatedHandle}`
+            }
+          }
+        },
+      });
+
+      // If this works then return the generated one.
+      handle = generatedHandle;
+      break;
+    } catch {
+      if (errorCount === 0) break;
+      errorCount--;
+      // Add error reporting here to show how often this happens
+    }
+  }
+};
 
 export const authOptions: NextAuthOptions = {
+  events: {
+    async signIn({ isNewUser, user }) {
+      // Handle first signin
+      if (isNewUser) {
+        await handleNewUser(user);
+      }
+    },
+  },
   callbacks: {
-    session({ session, user }) {
+    // @TODO: Use this to handle limited signup
+    signIn({ user }) {
+      if (!user.email) return false;
+      return allowedEmails.includes(user.email);
+    },
+    async session({ session, user }) {
       // Include user.id on session
       if (session.user) {
         session.user.id = user.id;
+        const databaseUser = await prisma.user.findUnique({
+          where: {
+            id: user.id,
+          },
+          include: {
+            page: true
+          }
+        });
+
+        if (databaseUser?.handle)
+          session.user.handle = databaseUser.handle;
+
+        if (databaseUser?.page)
+          session.user.page = databaseUser.page;
       }
 
       return session;
