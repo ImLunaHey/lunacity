b252a2bbe2cab19c3da7ca120c9d30cd4e237780
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-14 09:26:29 +1030

    chore: add snapshot test for tag-cloud component

---
+++ __tests__/components/tag-cloud.test.tsx
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { TagCloud } from '../../src/components/tag-cloud';
+
+describe('TagCloud', () => {
+  it('it renders an error message', () => {
+    const { container } = render(
+      <TagCloud
+        tags={[
+          {
+            id: '123',
+            name: 'testing',
+          },
+        ]}
+      />
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
--- src/components/tag-cloud.tsx
+++ src/components/tag-cloud.tsx
@@ -1,9 +1,13 @@
 import { Badge, Link } from '@nextui-org/react';
 
-export const TagCloud: React.FC<{ tags: { id: string; name: string; }[] }> = ({ tags }) => {
+export const TagCloud: React.FC<{ tags: { id: string; name: string }[] }> = ({ tags }) => {
   return (
     <>
-      {(tags).map(tag => <Badge isSquared key={tag.name}><Link className="text-white" href={`/tags/${tag.name}`}>#{tag.name}</Link></Badge>)}
+      {tags.map((tag) => (
+        <Badge isSquared key={tag.name}>
+          <Link className="text-white" href={`/tags/${tag.name}`}>{`#${tag.name}`}</Link>
+        </Badge>
+      ))}
     </>
   );
 };
