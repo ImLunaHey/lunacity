d4f7b2c4b83924a085a293792a92695c2aa78b14
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-15 00:31:28 +1030

    chore: add followed-notification component tests

---
+++ __tests__/components/notifications/followed-notification.test.tsx
@@ -0,0 +1,36 @@
+import { FollowedNotification } from '@app/components/notifications/followed-notification';
+import { render } from '@testing-library/react';
+
+import type { Notification } from '@app/stores/notification';
+
+import '@testing-library/jest-dom';
+
+jest.mock('react-i18next', () => ({
+    useTranslation: jest.fn(() => ({
+        t: jest.fn((text: string) => text)
+    })),
+}));
+
+describe('FollowedNotification', () => {
+    it('renders nothing if no data is provided', () => {
+        const { container } = render(<FollowedNotification notification={null} />);
+        expect(container).toMatchSnapshot();
+    });
+
+    it('returns x followed you', () => {
+        const notification = {
+            data: {
+                page: {
+                    handle: 'test-page',
+                }
+            },
+            id: '123',
+            read: false,
+            type: 'FOLLOWED_BACK',
+            notifiedId: '456'
+        } as Notification;
+
+        const { container } = render(<FollowedNotification notification={notification} />);
+        expect(container).toMatchSnapshot();
+    });
+});
--- src/components/notifications/followed-notification.tsx
+++ src/components/notifications/followed-notification.tsx
@@ -1,18 +1,22 @@
 import Link from 'next/link';
+
 import type { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import type { Notification } from '@app/stores/notification';
 
 // Fix the padding and such
-export const FollowedNotification: FC<{ notification: Notification }> = ({ notification: { data } }) => {
+export const FollowedNotification: FC<{ notification: Notification | null }> = ({ notification }) => {
   const { t } = useTranslation();
 
-  // TODO: Fix this 
-  if (!data?.page?.handle) return null;
+  // If we forgot to pass data don't render
+  if (!notification) return null;
+
+  // TODO: Fix this
+  if (!notification.data?.page?.handle) return null;
 
   return (
-    <Link href={`/@${data.page?.handle}`} className="text-xs text-gray-500 hover:text-white">
-      {data.page?.displayName ?? `@${data.page?.handle}`} {t('followed-you')}
+    <Link href={`/@${notification.data.page?.handle}`} className="text-xs text-gray-500 hover:text-white">
+      {notification.data.page?.displayName ?? `@${notification.data.page?.handle}`} {t('followed-you')}
     </Link>
   );
 };
