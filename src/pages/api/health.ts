62a0033d18ef0f05a94ab6e0ef5a4ff6c5bcc14e
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-14 21:10:39 +1030

    fix: health endpoint not ending response

--- src/pages/api/health.ts
+++ src/pages/api/health.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 const Health = (_request: NextApiRequest, response: NextApiResponse) => {
-    response.status(200);
+    response.status(200).end();
 };
 
 export default Health;
