7de599d2206e90d0438304f0636985363399c3a9
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-15 00:12:44 +1030

    feat: add russian time-ago locale

--- src/common/human-time.ts
+++ src/common/human-time.ts
@@ -1,7 +1,10 @@
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en-AU';
+import ru from 'javascript-time-ago/locale/ru';
 
 TimeAgo.addLocale(en);
+TimeAgo.addLocale(ru);
+TimeAgo.addDefaultLocale(en);
 
 export const humanTime = (timestamp: Date, locale = 'en-AU') => {
   const timeAgo = new TimeAgo(locale);
