c283927cdae5a071ceff98223eea4e93b3b0be5b
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-15 00:12:23 +1030

    chore: add more humanTime tests

--- __tests__/common/human-time.test.ts
+++ __tests__/common/human-time.test.ts
@@ -7,4 +7,19 @@ describe('humanTime', () => {
         const time = humanTime(new Date(), 'en-AU');
         expect(time).toBe('now');
     });
+
+    it('returns english by default', () => {
+        const time = humanTime(new Date());
+        expect(time).toBe('now');
+    });
+
+    it('falls back to english if a locale hasn\'t been registered', () => {
+        const time = humanTime(new Date(), 'non-existant');
+        expect(time).toBe('now');
+    });
+
+    it('allows the locale to be set', () => {
+        const time = humanTime(new Date(), 'ru');
+        expect(time).toBe('только что');
+    });
 });
