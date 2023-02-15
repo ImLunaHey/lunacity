e94676d8bf5fd35ff10a7cdcf7eaefed2ae7e01b
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-15 14:51:57 +1030

    chore: rename withAuth -> withPrivateAccess

--- __tests__/common/with-auth.test.ts
+++ __tests__/common/with-auth.test.ts
@@ -1,4 +1,4 @@
-import { withAuth } from '@app/common/with-auth';
+import { withPrivateAccess } from '@app/common/with-private-access';
 import { getSession } from 'next-auth/react';
 
 import type { IncomingMessage, ServerResponse } from 'http';
@@ -7,11 +7,11 @@ import '@testing-library/jest-dom';
 
 jest.mock('next-auth/react');
 
-describe('withAuth', () => {
+describe('withPrivateAccess', () => {
     it('redirects the user when not-authenticated', async () => {
         jest.mocked(getSession).mockImplementation(() => new Promise(resolve => resolve(null)));
 
-        const fn = withAuth();
+        const fn = withPrivateAccess();
         await expect(fn({
             req: {} as IncomingMessage & { cookies: Partial<{ [key: string]: string; }>; },
             res: {} as ServerResponse<IncomingMessage>,
@@ -33,7 +33,7 @@ describe('withAuth', () => {
             expires: new Date().toISOString()
         })));
 
-        const fn = withAuth();
+        const fn = withPrivateAccess();
         await expect(fn({
             req: {} as IncomingMessage & { cookies: Partial<{ [key: string]: string; }>; },
             res: {} as ServerResponse<IncomingMessage>,
--- src/common/with-private-access.ts
+++ src/common/with-private-access.ts
@@ -1,10 +1,11 @@
 import type { Dict } from '@trpc/server';
 import type { GetServerSideProps, GetServerSidePropsContext } from 'next'
+import { Session } from 'next-auth';
 import { getSession } from 'next-auth/react';
 
-type Callback = (context: Parameters<GetServerSideProps>[0]) => ReturnType<GetServerSideProps> | Awaited<ReturnType<GetServerSideProps>>;
+type Callback = (context: Parameters<GetServerSideProps>[0] & { session: Session }) => ReturnType<GetServerSideProps> | Awaited<ReturnType<GetServerSideProps>>;
 
-export const withAuth = <Props extends Dict<string | string[]>, T extends Callback = Callback>(fn?: T) => {
+export const withPrivateAccess = <Props extends Dict<string | string[]>, T extends Callback = Callback>(fn?: T) => {
     return async (context: GetServerSidePropsContext<Props>) => {
         const session = await getSession({ req: context.req });
         if (!session) {
@@ -15,7 +16,6 @@ export const withAuth = <Props extends Dict<string | string[]>, T extends Callba
                 },
             };
         }
-
-        return (fn?.(context) ?? { props: {} }) as ReturnType<T>;
+        return (fn?.({ ...context, session }) ?? { props: { session } }) as ReturnType<T>;
     };
 };
