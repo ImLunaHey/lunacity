3e047e1434e3d1720c5f986b8a68945b5bb388b5
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-15 21:20:27 +1030

    chore: move message methods to methodService

--- src/server/api/routers/message.ts
+++ src/server/api/routers/message.ts
@@ -1,22 +1,9 @@
+import { messageService } from '@app/services/message-service';
 import { createTRPCRouter, protectedProcedure } from '../trpc';
 
 export const messageRouter = createTRPCRouter({
   getAllMessageThreads: protectedProcedure
-    .query(({ ctx: { prisma, session } }) => {
-      return prisma?.messageThread.findMany({
-        where: {
-          participants: {
-            some: {
-              owner: {
-                id: session.user.id
-              }
-            }
-          }
-        },
-        include: {
-          messages: true,
-          participants: true
-        }
-      });
+    .query(({ ctx }) => {
+      return messageService.getAllMessageThreads(ctx);
     }),
 });
---
+++ src/services/message-service.ts
@@ -0,0 +1,23 @@
+import type { PrivateServiceContext } from '@app/types/service';
+
+class MessageService {
+    async getAllMessageThreads(ctx: PrivateServiceContext) {
+        return prisma?.messageThread.findMany({
+            where: {
+                participants: {
+                    some: {
+                        owner: {
+                            id: ctx.session.user?.id
+                        }
+                    }
+                }
+            },
+            include: {
+                messages: true,
+                participants: true
+            }
+        });
+    }
+}
+
+export const messageService = new MessageService();
