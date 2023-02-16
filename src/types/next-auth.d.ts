155e29764c2474af8bf690f5a4da3b06be18d889
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-16 19:21:42 +1030

    fix: make session always have a user

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,15 +1,14 @@
 import { type Page } from '@prisma/client';
-import { type DefaultSession } from 'next-auth';
 
 declare module 'next-auth' {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user?: {
+    user: {
       id: string;
       handle?: string;
       page?: Page
-    } & DefaultSession['user'];
+    };
   }
 }
