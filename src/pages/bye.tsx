e2ca592d9324d58343054fa858d9d6d97d4bbdc1
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-17 02:01:59 +1030

    chore: add goodye text to deactivate page

--- src/pages/bye.tsx
+++ src/pages/bye.tsx
@@ -1,5 +1,14 @@
 import { type FC } from 'react';
 
-const Bye: FC = () => <div>Bye 👋</div>;
+const Bye: FC = () => (
+  <div>
+    <h1>Account Deactivated</h1>
+    <p>Your account has been deactivated.</p>
+    <p>
+      In 30 days your account will be deleted. If you would like to reactivate your account before that happens, just
+      signin again.
+    </p>
+  </div>
+);
 
 export default Bye;
