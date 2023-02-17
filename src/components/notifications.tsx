4a75b19af37697ca63738642174e5189a6e1a94f
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-18 02:37:57 +1030

    chore: move notifications to client only

--- src/components/notifications.tsx
+++ src/components/notifications.tsx
@@ -1,8 +1,12 @@
+'use client';
+
 import { LoadingSpinner } from '@app/components/loading-spinner';
 import { useNotificationStore } from '@app/stores/notification';
 import { Dropdown, Navbar, Badge, styled } from '@nextui-org/react';
 import { useSession } from 'next-auth/react';
 import { type FC, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
 import { NotificationIcon } from '../icons/notification-icon';
 import { api } from '../utils/api';
 import { FollowedNotification } from './notifications/followed-notification';
@@ -17,10 +21,12 @@ const NotificationButton = styled('button', {
 });
 
 export const Notifications: FC = () => {
+  const { t } = useTranslation(['common']);
   const { data: session } = useSession();
   const [isLoading, setIsLoading] = useState(true);
   const notifications = useNotificationStore((state) => state.notifications);
   const addNotifications = useNotificationStore((state) => state.addNotifications);
+  const addNotification = useNotificationStore((state) => state.addNotification);
   const handleSelection = () => undefined;
 
   // Get all the current notifications from the server
@@ -34,6 +40,26 @@ export const Notifications: FC = () => {
     }
   );
 
+  api.notification.getLiveNotifications.useSubscription(undefined, {
+    onData({ notification }) {
+      // @TODO: This should be a jsx per type
+      if (!notification) return;
+
+      // Save the notification to the store
+      addNotification(notification);
+
+      if (notification.type === 'FOLLOWED')
+        toast(<FollowedNotification notification={notification} />, { delay: 1000 });
+    },
+    onStarted() {
+      if (process.env.NODE_ENV === 'development') toast.info(t('notifications.live-notifications-started'));
+    },
+    onError(error) {
+      toast.error(error.message);
+    },
+    enabled: session?.user?.id !== undefined,
+  });
+
   // Don't render unless the user has a page
   if (!session?.user?.page?.handle) return null;
 
@@ -45,7 +71,7 @@ export const Notifications: FC = () => {
         <Navbar.Item>
           <Dropdown.Trigger>
             <NotificationButton aria-label={`${notifications.length} notifications`}>
-              {/* @TODO: the content and above label should relfect the actual count, if above 99 show 99+ */}
+              {/* @TODO: the content and above label should reflect the actual count, if above 99 show 99+ */}
               <Badge color="error" content={notifications.length} shape="circle" size="sm">
                 <NotificationIcon fill="currentColor" width={30} height={25} />
               </Badge>
