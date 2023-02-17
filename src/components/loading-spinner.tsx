ceae6743ba5c5512169fd69fa5cf0fd9e831fb67
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-17 22:27:15 +1030

    chore: make sure loading spinner is span

--- src/components/loading-spinner.tsx
+++ src/components/loading-spinner.tsx
@@ -2,5 +2,5 @@ import type { FC } from 'react';
 import { Loading } from '@nextui-org/react';
 
 export const LoadingSpinner: FC = () => {
-  return <Loading type="points-opacity" color="currentColor" size="sm" />;
+  return <Loading as="span" type="points-opacity" color="currentColor" size="sm" />;
 };
