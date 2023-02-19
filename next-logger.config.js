b3628ea25283f6b6be956492d0d732b4080c0eb4
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-19 23:10:16 +1030

    chore: remove logflare

--- next-logger.config.js
+++ next-logger.config.js
@@ -1,6 +1,10 @@
-import pino from 'pino';
+const pino = require( 'pino');
 
 // create pino logger
 export const logger = pino({
     level: process.env.LOG_LEVEL || 'info',
 });
+
+module.exports = {
+  logger,
+}
