e053a59032852e78f8f83cc4b9eb17a8be7df501
ImLunaHey <105922175+ImLunaHey@users.noreply.github.com>
2023-02-20 01:02:26 +1030

    feat: allow searching for posts

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,6 +1,8 @@
 import { withPrivateAccess } from '@app/common/with-private-access';
 import { LoadingSpinner } from '@app/components/loading-spinner';
+import { PageProfileCard } from '@app/components/page-profile-card';
 import { api } from '@app/utils/api';
+import type { Page, Post } from '@prisma/client';
 import Head from 'next/head';
 import type { FC } from 'react';
 import { useTranslation } from 'react-i18next';
@@ -11,20 +13,49 @@ export const getServerSideProps = withPrivateAccess<{ query: string }>((context)
   },
 }));
 
+const PageResult: FC<{ page: Page }> = ({ page }) => {
+  return <PageProfileCard {...page} />;
+};
+
+const PostResult: FC<{ post: Post }> = ({ post }) => {
+  return <pre>{JSON.stringify(post, null, 2)}</pre>;
+};
+
 const Search: FC<{ query: string }> = ({ query }) => {
   const results = api.search.query.useQuery({ query });
   const { t } = useTranslation();
 
   if (results.isLoading) return <LoadingSpinner />;
 
+  // No results found
+  if (results.data?.length === 0)
+    return (
+      <>
+        <Head>
+          <title>{t('search-results-for-query', { query })}</title>
+        </Head>
+        <div>{t('search-results-for-query', { query })}</div>
+        <div>{t('no-results-found')}</div>
+      </>
+    );
+
+  // Found results
   return (
     <>
       <Head>
         <title>{t('search-results-for-query', { query })}</title>
       </Head>
       <div>{t('search-results-for-query', { query })}</div>
-      {results.data?.length === 0 && <div>{t('no-results-found')}</div>}
-      {results.data && results.data?.length >= 1 && <pre>{JSON.stringify(results.data, null, 2)}</pre>}
+      {results.data?.map((result) => {
+        switch (result.type) {
+          case 'page':
+            return <PageResult page={result.data} />;
+          case 'post':
+            return <PostResult post={result.data} />;
+          default:
+            return null;
+        }
+      })}
     </>
   );
 };
--- src/server/api/routers/search.ts
+++ src/server/api/routers/search.ts
@@ -4,19 +4,34 @@ import { createTRPCRouter, protectedProcedure } from '@app/server/api/trpc';
 export const searchRouter = createTRPCRouter({
   query: protectedProcedure
     .input(z.object({ query: z.string() }))
-    .query(({ ctx: { prisma }, input: { query } }) => {
-      return prisma?.page.findMany({
-        where: {
-          OR: [{
-            displayName: {
-              contains: query
-            }
-          }, {
-            handle: {
-              contains: query
-            }
-          }]
-        }
-      });
+    .query(({ ctx, input }) => {
+      return ctx.prisma.$transaction([
+        ctx.prisma.page.findMany({
+          where: {
+            OR: [{
+              displayName: {
+                contains: input.query
+              }
+            }, {
+              handle: {
+                contains: input.query
+              }
+            }]
+          },
+        }),
+        ctx.prisma.post.findMany({
+          where: {
+            OR: [{
+              title: {
+                contains: input.query
+              }
+            }, {
+              body: {
+                contains: input.query
+              }
+            }]
+          }
+        })
+      ]).then(([pages, posts]) => [...pages.map(data => ({ type: 'page' as const, data })), ...posts.map(data => ({ type: 'post' as const, data }))]);
     }),
 });
